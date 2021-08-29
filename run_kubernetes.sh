f9b8acdbf966eb0f9245c344668efcf8aa54ab85
peter <ocanseypeter@gmail.com>
2021-08-29 09:34:03 +0000

    feat: worked on kubnetes deployment

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,20 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=ocanseypeter/uda_microserviceapp
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run kub-udas-microserviceapp\
+    --image=$dockerpath\
+    --port=80\
+    --labels app=kub-udas-microserviceapp
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward kub-udas-microserviceapp 8000:80
 
